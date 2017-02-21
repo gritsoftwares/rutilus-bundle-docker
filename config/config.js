4245678472c31380bbd99d233b556c1cb892aacd
henrique <henriquesc@gmail.com>
2017-02-21 15:22:14 -0500

    dockerfile

--- config/config.js
+++ config/config.js
@@ -39,7 +39,7 @@ module.exports = {
             },
             file: {
                 level: 'info',
-                file: 'loggerApi.log'
+                file: `/logs/loggerApi.log`
             }
         },
 
@@ -49,7 +49,7 @@ module.exports = {
             },
             file: {
                 level: 'info',
-                file: 'analyticsApi.log'
+                file: `/logs/analyticsApi.log`
             }
         },
     },
