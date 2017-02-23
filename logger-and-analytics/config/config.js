06227aaeb1b5d5eebaaed48f1cf60e29ac1bb668
Matthew Welke <mattwelke@gmail.com>
2017-02-22 19:30:43 -0500

    .

--- logger-and-analytics/config/config.js
+++ logger-and-analytics/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     addresses: {
-        database: `${ENV.MONGO_PORT}`.replace('tcp://', 'mongodb://'),
+        database: `${ENV.MONGO_PORT.replace('tcp://', 'mongodb://')}/production`,
         analytics: `${rutilusAddress || 'http://localhost'}:3000`,
     },
 
